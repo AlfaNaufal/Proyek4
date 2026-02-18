bd9870fa65d17632f9556a7522cdc914d1c63208
Naufal Alfa <108531196+AlfaNaufal@users.noreply.github.com>
2026-02-18 21:22:22 +0700

    Modul2_Homework

--- logbook_app_001/lib/features/onboarding/onboarding_view.dart
+++ logbook_app_001/lib/features/onboarding/onboarding_view.dart
@@ -9,19 +9,36 @@ class OnboardingView extends StatefulWidget {
 
 class _OnboardingViewState extends State<OnboardingView> {
   int _step = 1;
+  String pict = 'lib/assets/ichigo.jpg';
+  List<String> text = [
+    "Selamat datang di Counter!!!",
+    "Counter siap menghitung!!",
+    "Anda bisa menambah dan mengurangi angka!!",
+  ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.greenAccent,
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Text("Halaman Onboarding", style: TextStyle(fontSize: 20)),
-            Text('$_step', style: TextStyle(fontSize: 50)),
+            Text("Aplikasi Counter", style: TextStyle(fontSize: 20)),
+            SizedBox(
+              width: MediaQuery.widthOf(context) / 2,
+              child: Image.asset(pict, fit: BoxFit.contain),
+            ),
+            Text('${text[_step - 1]}', style: TextStyle(fontSize: 16)),
             ElevatedButton(
               onPressed: () {
-                if (_step < 2) {
+                if (_step < 3) {
+                  if (_step == 1) {
+                    pict = 'lib/assets/Nigo.jpg';
+                  }
+                  if (_step == 2) {
+                    pict = 'lib/assets/both.jpg';
+                  }
                   setState(() {
                     _step++;
                   });
@@ -40,3 +57,4 @@ class _OnboardingViewState extends State<OnboardingView> {
     );
   }
 }
+
